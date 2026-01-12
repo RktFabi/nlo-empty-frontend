8d148e882bd7d8e8935a0b10afeaa798c7cb19ec
nlo-danielchoi <daniel.choi@needlist.org>
2026-01-11 23:51:15 -0500

    Add admin needlist table route

--- src/routes/admin/needlist-table-view.tsx
+++ src/routes/admin/needlist-table-view.tsx
@@ -1,9 +1,7 @@
+// Registers the admin needlist table route using the NeedlistTableView page.
+import { NeedlistTableView } from '@/pages/admin/NeedlistTableView';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/admin/needlist-table-view')({
-  component: RouteComponent,
+  component: NeedlistTableView,
 });
-
-function RouteComponent() {
-  return <div>Hello "/admin/needlist-table-view"!</div>;
-}
