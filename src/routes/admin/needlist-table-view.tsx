629a0331eb281645c6d1ca100fb12642e08cb949
brigittewylee <brigitte.leewy@gmail.com>
2026-01-12 11:53:16 -0500

    adds Table Search/Filter bar

--- src/routes/admin/needlist-table-view.tsx
+++ src/routes/admin/needlist-table-view.tsx
@@ -2,6 +2,7 @@ import { StatusBadge } from '@/components/StatusBadge';
 import { createFileRoute } from '@tanstack/react-router';
 import { PageBackground } from '@/components/PageBackground';
 import { NeedlistTable } from '@/features/admin/needlist-table-view/NeedlistTable';
+import { NeedlistTableFilters } from '@/features/admin/needlist-table-view/NeedlistTableFilters';
 
 export const Route = createFileRoute('/admin/needlist-table-view')({
   component: RouteComponent,
@@ -12,6 +13,7 @@ function RouteComponent() {
   <div>
     <PageBackground />
     <NeedlistTable />
+    <NeedlistTableFilters />
     Hello "/admin/needlist-table-view"!
   </div>);
 }
